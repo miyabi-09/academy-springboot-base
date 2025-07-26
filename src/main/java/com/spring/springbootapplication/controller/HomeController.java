c13479c762970e2f436ec89883dd81c8e210ed3b
miyabi otsuka <miyabi@miyabinoMacBook-Air.local>
2025-07-26 19:23:42 +0900

    最新の変更をコミット

--- src/main/java/com/spring/springbootapplication/controller/HomeController.java
+++ src/main/java/com/spring/springbootapplication/controller/HomeController.java
@@ -1,13 +1,13 @@
-package com.spring.springbootapplication.controller;
+//package com.spring.springbootapplication.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HomeController {
+//@RestController
+//public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Hello!";
-    }
-}
+    //@GetMapping("/")
+    //public String home() {
+        //return "index";  // templates/index.html を返す
+    //}
+//}
