d1c9bbaa40b522cd272b6e35d032d0293393d3e0
miyabi otsuka <miyabi@miyabinoMacBook-Air.local>
2025-07-27 18:52:56 +0900

    更新

--- src/main/java/com/spring/springbootapplication/dto/UserForm.java
+++ src/main/java/com/spring/springbootapplication/dto/UserForm.java
@@ -9,15 +9,15 @@ import jakarta.validation.constraints.Size;
 
 public class UserForm {
 
-    @NotBlank(message = "氏名は必須です")
+    @NotBlank(message = "氏名は必ず入力してください")
     @Size(max = 255, message = "氏名は255文字以内で入力してください")
     private String name;
 
-    @NotBlank(message = "メールアドレスは必須です")
+    @NotBlank(message = "メールアドレスは必ず入力してください")
     @Email(message = "メールアドレスの形式が正しくありません")
     private String email;
 
-    @NotBlank(message = "パスワードは必須です")
+    @NotBlank(message = "パスワードは必ず入力してください")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "英数字8文字以上で入力してください")
     private String password;
 
