bf2a719f85e5888cef7f31443434c37f2e3a8a20
miyabi otsuka <miyabi@miyabinoMacBook-Air.local>
2025-07-27 21:38:46 +0900

    【個人開発5】新規登録画面の追加

--- src/main/java/com/spring/springbootapplication/controller/HomeController.java
+++ src/main/java/com/spring/springbootapplication/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.spring.springbootapplication.controller;
+//package com.spring.springbootapplication.controller;
 
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RestController;
