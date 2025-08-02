3538c967a61df7bbaf64e91f45f8ac7dd76c625d
miyabi otsuka <miyabi@miyabinoMacBook-Air.local>
2025-08-02 15:41:19 +0900

    Fix Flyway conflict

--- src/main/java/com/spring/springbootapplication/entity/User.java
+++ src/main/java/com/spring/springbootapplication/entity/User.java
@@ -51,7 +51,7 @@ public class User {
     private LocalDateTime updatedAt; // 更新日時
 
 
-    // ---- getter・setter ----
+    // --- getter・setter ---
     public Long getId() {
         return id;
     }
