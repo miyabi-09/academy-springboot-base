9633b4439b78104eb405a5792602041201814ad2
miyabi otsuka <miyabi.otsuka@prum.jp>
2025-08-21 20:36:24 +0900

    chore: 起動/認証の微修正とメール正規化

--- src/main/java/com/spring/springbootapplication/SpringbootApplication.java
+++ src/main/java/com/spring/springbootapplication/SpringbootApplication.java
@@ -2,9 +2,7 @@ package com.spring.springbootapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootApplication {
 
